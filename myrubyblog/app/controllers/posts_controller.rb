43015f669545ebf0e1089173fd7e70d571d6dd73
Tarang Barua <tarang727@gmail.com>
2017-10-15 02:57:57 +0530

    Commit

---
+++ myrubyblog/app/controllers/posts_controller.rb
@@ -0,0 +1,43 @@
+class PostsController < ApplicationController
+	def index
+		@posts = Post.all
+	end
+	
+	def new
+		@post = Post.new
+		@category = Category.all
+	end
+	
+	def create
+		@post = Post.new(params[:post])
+		if @post.save
+			redirect_to posts_path, :notice => "Your post has been saved"
+		else
+			render "new"
+		end
+	end
+	
+	def edit
+		@post = Post.find(params[:id])
+	end	
+	
+	def update
+		@post = Post.find(params[:id])
+		if @post.update_attributes(params[:post])
+			redirect_to posts_path, :notice => "Your post has been updated"
+		else
+			render "edit"
+		end
+	end
+	
+	def show
+	   @post = Post.find(params[:id])
+	end
+	
+	def destroy
+		@post = Post.find(params[:id])
+		@post.destroy
+		redirect_to posts_path, :notice => "Your post has been deleted"
+	end
+	
+end
